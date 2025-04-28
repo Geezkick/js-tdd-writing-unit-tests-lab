e5ba06ccf249f172df08217b370afe673619a6e2
Brian <nyarienyabrian05@gmail.com>
2025-04-28 19:46:59 +0300

    Fix isPalindrome to handle empty string and remove jest.Configure.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,31 @@
-// Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  test('should return true for a palindrome like "racecar"', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  test('should return false for a non-palindrome like "car"', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  test('should return true for mixed case palindromes like "Racecar"', () => {
+    expect(isPalindrome('Racecar')).toBe(true);
+  });
+
+  test('should return false for an empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  test('should throw an error for non-alphabetic characters', () => {
+    expect(() => isPalindrome('racecar123')).toThrow('Input must contain only alphabetic characters');
+  });
+
+  test('should throw an error for non-string input', () => {
+    expect(() => isPalindrome(123)).toThrow('Input must be a string');
+  });
+
+  test('should return true for a single character like "a"', () => {
+    expect(isPalindrome('a')).toBe(true);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
-// Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+    if (word === '') {
+      return false;
+    }
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    const lowerWord = word.toLowerCase();
+    const len = lowerWord.length;
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+      if (lowerWord[i] !== lowerWord[len - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  module.exports = { isPalindrome };
